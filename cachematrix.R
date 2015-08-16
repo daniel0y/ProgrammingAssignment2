41b164542c1133c1bfe6a4adf85acc6bfb84a8ba
daniel0y <uki0713@gmail.com>
2015-08-16 10:18:40 +0800

    bug fix

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   set.inv <- function(inverse) inv <<- inverse
   get.inv <- function() inv
-  list(set = set, get = get, setmean = setmean, getmean = getmean)
+  list(set = set, get = get, set.inv = set.inv, get.inv = get.inv)
 }
 
 
